72395654507aec5054b93baff8a0d07de9131b1d
pater18 <55958169+pater18@users.noreply.github.com>
2020-10-07 21:03:18 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,192 +1,164 @@
-#include <gazebo/gazebo_client.hh>
-#include <gazebo/msgs/msgs.hh>
-#include <gazebo/transport/transport.hh>
-
-#include "fl/Headers.h"
-
-#include <opencv2/opencv.hpp>
-
+#include <opencv4/opencv2/core/core.hpp>
+#include <opencv4/opencv2/imgproc.hpp>
+#include <opencv4/opencv2/highgui.hpp>
 #include <iostream>
+#include <iomanip>
+#include <vector>
+//#include <stdlib.h>
 
-static boost::mutex mutex;
+const cv::Vec3b black {0,0,0};
+const cv::Vec3b white {255, 255, 255};
 
-void statCallback(ConstWorldStatisticsPtr &_msg) {
-  (void)_msg;
-  // Dump the message contents to stdout.
-  //  std::cout << _msg->DebugString();
-  //  std::cout << std::flush;
-}
+using namespace std;
 
-void poseCallback(ConstPosesStampedPtr &_msg) {
-  // Dump the message contents to stdout.
-  //  std::cout << _msg->DebugString();
+int main()
+{
+    std::vector<std::vector<long int>> brushValues;
 
-  for (int i = 0; i < _msg->pose_size(); i++) {
-    if (_msg->pose(i).name() == "pioneer2dx") {
-
-      std::cout << std::setprecision(2) << std::fixed << std::setw(6)
-                << _msg->pose(i).position().x() << std::setw(6)
-                << _msg->pose(i).position().y() << std::setw(6)
-                << _msg->pose(i).position().z() << std::setw(6)
-                << _msg->pose(i).orientation().w() << std::setw(6)
-                << _msg->pose(i).orientation().x() << std::setw(6)
-                << _msg->pose(i).orientation().y() << std::setw(6)
-                << _msg->pose(i).orientation().z() << std::endl;
-    }
-  }
-}
-
-void cameraCallback(ConstImageStampedPtr &msg) {
-
-  std::size_t width = msg->image().width();
-  std::size_t height = msg->image().height();
-  const char *data = msg->image().data().c_str();
-  cv::Mat im(int(height), int(width), CV_8UC3, const_cast<char *>(data));
-
-  im = im.clone();
-  cv::cvtColor(im, im, cv::COLOR_RGB2BGR);
-
-  mutex.lock();
-  cv::imshow("camera", im);
-  mutex.unlock();
-}
-
-void lidarCallback(ConstLaserScanStampedPtr &msg) {
-
-  //  std::cout << ">> " << msg->DebugString() << std::endl;
-  float angle_min = float(msg->scan().angle_min());
-  //  double angle_max = msg->scan().angle_max();
-  float angle_increment = float(msg->scan().angle_step());
-
-  float range_min = float(msg->scan().range_min());
-  float range_max = float(msg->scan().range_max());
-
-  int sec = msg->time().sec();
-  int nsec = msg->time().nsec();
-
-  int nranges = msg->scan().ranges_size();
-  int nintensities = msg->scan().intensities_size();
-
-  assert(nranges == nintensities);
-
-  int width = 400;
-  int height = 400;
-  float px_per_m = 200 / range_max;
-
-  cv::Mat im(height, width, CV_8UC3);
-  im.setTo(0);
-  for (int i = 0; i < nranges; i++) {
-    float angle = angle_min + i * angle_increment;
-    float range = std::min(float(msg->scan().ranges(i)), range_max);
-    //    double intensity = msg->scan().intensities(i);
-    cv::Point2f startpt(200.5f + range_min * px_per_m * std::cos(angle),
-                        200.5f - range_min * px_per_m * std::sin(angle));
-    cv::Point2f endpt(200.5f + range * px_per_m * std::cos(angle),
-                      200.5f - range * px_per_m * std::sin(angle));
-    cv::line(im, startpt * 16, endpt * 16, cv::Scalar(255, 255, 255, 255), 1,
-             cv::LINE_AA, 4);
-
-    //    std::cout << angle << " " << range << " " << intensity << std::endl;
-  }
-  cv::circle(im, cv::Point(200, 200), 2, cv::Scalar(0, 0, 255));
-  cv::putText(im, std::to_string(sec) + ":" + std::to_string(nsec),
-              cv::Point(10, 20), cv::FONT_HERSHEY_PLAIN, 1.0,
-              cv::Scalar(255, 0, 0));
-
-  mutex.lock();
-  cv::imshow("lidar", im);
-  mutex.unlock();
-}
-
-int main(int _argc, char **_argv) {
-
-
-    //using namespace fl;
-    fl::Engine* engine = fl::FllImporter().fromFile("ObstacleAvoidance.fll");
-
-    std::string status;
-    if (not engine->isReady(&status))
-        throw fl::Exception("[engine error] engine is not ready:n" + status, FL_AT);
-
-    fl::InputVariable* obstacle = engine->getInputVariable("obstacle");
-    fl::OutputVariable* steer = engine->getOutputVariable("mSteer");
-
-
-  // Load gazebo
-  gazebo::client::setup(_argc, _argv);
-
-  // Create our node for communication
-  gazebo::transport::NodePtr node(new gazebo::transport::Node());
-  node->Init();
-
-  // Listen to Gazebo topics
-  gazebo::transport::SubscriberPtr statSubscriber =
-      node->Subscribe("~/world_stats", statCallback);
-
-  gazebo::transport::SubscriberPtr poseSubscriber =
-      node->Subscribe("~/pose/info", poseCallback);
-
-  gazebo::transport::SubscriberPtr cameraSubscriber =
-      node->Subscribe("~/pioneer2dx/camera/link/camera/image", cameraCallback);
-
-  gazebo::transport::SubscriberPtr lidarSubscriber =
-      node->Subscribe("~/pioneer2dx/hokuyo/link/laser/scan", lidarCallback);
-
-  // Publish to the robot vel_cmd topic
-  gazebo::transport::PublisherPtr movementPublisher =
-      node->Advertise<gazebo::msgs::Pose>("~/pioneer2dx/vel_cmd");
-
-  // Publish a reset of the world
-  gazebo::transport::PublisherPtr worldPublisher =
-      node->Advertise<gazebo::msgs::WorldControl>("~/world_control");
-  gazebo::msgs::WorldControl controlMessage;
-  controlMessage.mutable_reset()->set_all(true);
-  worldPublisher->WaitForConnection();
-  worldPublisher->Publish(controlMessage);
-
-  const int key_left = 81;
-  const int key_up = 82;
-  const int key_down = 84;
-  const int key_right = 83;
-  const int key_esc = 27;
-
-  float speed = 0.0;
-  float dir = 0.0;
-
-  // Loop
-  while (true) {
-    gazebo::common::Time::MSleep(10);
-
-    mutex.lock();
-    int key = cv::waitKey(1);
-    mutex.unlock();
-
-    if (key == key_esc)
-      break;
-
-    if ((key == key_up) && (speed <= 1.2f))
-      speed += 0.05;
-    else if ((key == key_down) && (speed >= -1.2f))
-      speed -= 0.05;
-    else if ((key == key_right) && (dir <= 0.4f))
-      dir += 0.05;
-    else if ((key == key_left) && (dir >= -0.4f))
-      dir -= 0.05;
-    else {
-      // slow down
-      //      speed *= 0.1;
-      //      dir *= 0.1;
+    cv::Mat image = cv::imread("floor_plan.jpg");
+    if (image.empty()){
+        std::cerr << "Could not load image";
+        return 1;
     }
 
-    // Generate a pose
-    ignition::math::Pose3d pose(double(speed), 0, 0, 0, 0, double(dir));
+    int x = image.cols;
+    int y = image.rows;
 
-    // Convert to a pose message
-    gazebo::msgs::Pose msg;
-    gazebo::msgs::Set(&msg, pose);
-    movementPublisher->Publish(msg);
-  }
+    // Assing each pixel a value increasing from a wall from left to right
+    int startFromWall = 2;
+    for (int j = 0; j < y; j++){
+        std::vector<long int> tempRows; // Temporary vector to contain values of each row.
+        for (int i = 0; i < x; i++) {
 
-  // Make sure to shut everything down.
-  gazebo::client::shutdown();
+            if (image.at<cv::Vec3b>(j, i) != black){
+                tempRows.push_back(startFromWall++);
+            } else if (image.at<cv::Vec3b>(j, i) == black){
+                tempRows.push_back(0);
+                startFromWall = 2;
+            }
+        }
+        brushValues.push_back(tempRows);
+    }
+    int index = 2;
+    //Kigger fra venstre mod højre
+    for (int j = 0; j < y; j++){
+        for (int i = 0; i < x; i++) {
+            if (brushValues[j][x - 1 - i] != 0 && brushValues[j][image.cols - 1 - i] > index){
+                brushValues[j][x - 1 - i] = index++;
+            } else if (brushValues[j][x - 1 - i] == 0){
+                index = 2;
+            }
+        }
+    }
+    index = 2;
+    //Kigger fra top til bund
+    for (int j = 0; j < image.cols; j++){
+        for (int i = 0; i < image.rows; i++) {
+            if (brushValues[i][j] != 0 && brushValues[i][j] > index){
+                brushValues[i][j] = index++;
+            } else if (brushValues[i][j] == 0){
+                index = 2;
+            }
+        }
+    }
+    index = 2;
+    //Kigger fra bund til top
+    for (int j = 0; j < image.cols; j++){
+        for (int i = 0; i < image.rows; i++) {
+            if (brushValues[image.rows - 1 - i][j] != 0 && brushValues[image.rows - 1 - i][j] > (index )){
+                brushValues[image.rows - 1 - i][j] = index++;
+            } else if (brushValues[image.rows - 1 - i][j] == 0){
+                index = 2;
+            }
+        }
+    }
+
+
+    // Shows the outut of the brushfire before correcting
+    for (size_t j = 0; j < brushValues.size(); j++){
+        for (size_t i = 0; i < brushValues[j].size(); i++){
+            std::cout << std::setw(3) << brushValues[j][i] ;
+        }
+        std::cout << "next line "<< std::endl;
+    }
+    std::cout << std::endl;
+
+    // Correction of numbers so they only jump one up or down in each direction
+    int value = 0;
+    while(value < 15){
+
+        for (int j = 0; j < image.cols; j++){
+
+            for (int i = 0; i < image.rows; i++) {
+                if (brushValues[i][j] != 0 && brushValues[i][j] != 2){
+                    if (abs(brushValues[i][j] - brushValues[i + 1][j]) >= 2 ){
+
+
+                        if (brushValues[i][j] - brushValues[i + 1][j] <= -2 ){
+                            brushValues[i][j]++;
+                        } else if (brushValues[i][j] - brushValues[i + 1][j] >= 2 ) {
+                            brushValues[i][j]--;
+                        }
+                    }
+
+                    if (abs(brushValues[i][j] - brushValues[i - 1][j]) >= 2 ){
+
+
+                        if (brushValues[i][j] - brushValues[i - 1][j] <= -2 ){
+                            brushValues[i][j]++;
+                        } else if (brushValues[i][j] - brushValues[i - 1][j] >= 2 ){
+                            brushValues[i][j]--;
+                        }
+                    }
+
+                    if (abs(brushValues[i][j] - brushValues[i][j + 1]) >= 2 ){
+
+                        if (brushValues[i][j] - brushValues[i][j + 1] <= -2 ){
+                            brushValues[i][j]++;
+                        } else if (brushValues[i][j] - brushValues[i][j + 1] >= 2 ){
+                            brushValues[i][j]--;
+                        }
+                    }
+
+                    if (abs(brushValues[i][j] - brushValues[i][j - 1]) >= 2 ){
+
+                        if (brushValues[i][j] - brushValues[i][j - 1] <= -2 ){
+                            brushValues[i][j]++;
+                        } else if (brushValues[i][j] - brushValues[i][j - 1] >= 2 ){
+                            brushValues[i][j]--;
+                        }
+                    }
+                }
+            }
+        }
+        value++;
+    }
+
+    //Show the result of the brushfire algorithme
+    for (size_t j = 0; j < brushValues.size(); j++){
+        for (size_t i = 0; i < brushValues[j].size(); i++){
+            std::cout << std::setw(3) << brushValues[j][i] ;
+        }
+        std::cout << "next line "<< std::endl;
+    }
+
+
+//    cv::Mat Brushfire (cv::Size(image),);
+//    Brushfire.resize(y,x);
+//    for (size_t j = 0; j < brushValues.size(); j++){
+//        for (size_t i = 0; i < brushValues[j].size(); i++){
+//            Brushfire.at<cv::Vec2b>(j, i) = black ;
+//        }
+//        std::cout << "next line "<< std::endl;
+//    }
+
+
+
+    std::cout << std::endl;
+
+    cv::namedWindow("Brushfire");
+
+    cv::imshow("Brushfire", image);
+    cv::waitKey(0);
+    return 0;
 }
